be3e7ec2a759b4373f2d2a859854a944ce8d4826
Soham Tamboli <sohamtamboli@gmail.com>
2021-05-23 02:19:46 +0530

    WIP: middlesection

--- src/App.js
+++ src/App.js
@@ -1,21 +1,26 @@
 import React from "react";
 import {
-  InfoCard,
-  CardContentWrapper,
-  SideBarWrapper,
-  SectionWrapper,
-  SectionHeadline,
-  CenteredCardText,
-  CardImage,
-  TrainingDetails,
-  HealthSectionItem,
-  InlineWrapper,
-  ActivityTimePlan,
-  ActivityMoreIcon,
-  Chip,
-  MoreWrapper,
-  Gradient,
-  UserDetails,
+	InfoCard,
+	CardContentWrapper,
+	SideBarWrapper,
+	SectionWrapper,
+	SectionHeadline,
+	CenteredCardText,
+	CardImage,
+	TrainingDetails,
+	HealthSectionItem,
+	InlineWrapper,
+	ActivityTimePlan,
+	ActivityMoreIcon,
+	Chip,
+	MoreWrapper,
+	Gradient,
+	UserDetails,
+	MiddleSectionWrapper,
+	SearchBarCalendarComp,
+	MiddleHeaderComp,
+	ActionCards,
+	TwoElementFlexComp,
 } from "./components/common";
 import DashboardLayout from "./layouts/dashboardLayout";
 import BellIcon from "./assets/images/bx-bell.svg";
@@ -28,134 +33,163 @@ import RegisterIcon from "./assets/images/undrawRegister.svg";
 import ellipsisIcon from "./assets/images/ellipsis.svg";
 import moreIcon from "./assets/images/more.svg";
 import avatarIcon from "./assets/images/man.svg";
+import fitnessImg from "./assets/images/fitnessIsometric.svg";
+import noticeImg from "./assets/images/notificationsIsometric.svg";
 function App() {
-  return (
-    <DashboardLayout>
-      <div>Left Section</div>
-      <div>Mid Section</div>
-      <>
-        <Gradient from={"#fff"} to={"#f8f9fd"}>
-          <SideBarWrapper>
-            <UserDetails
-              avatarSrc={avatarIcon}
-              name={"Floyd Miles"}
-              accountType={"Free account"}
-              iconOneSrc={BellIcon}
-              iconTwoSrc={SettingIcon}
-              badge={"1"}
-            />
+	return (
+		<DashboardLayout>
+			<div>Left Section</div>
+			<>
+				<MiddleSectionWrapper>
+					<SearchBarCalendarComp />
+					<MiddleHeaderComp />
+					<TwoElementFlexComp gap={"32px"}>
+						<ActionCards
+							src={fitnessImg}
+							msg={"This is Card One.This is Card One."}
+							btnTxt={"More Details"}
+							btnColor={"#FF7368"}
+							btnPadding={"1rem 1.2rem"}
+							btnFontSize={"1.2rem"}
+							itemAlignment={"flex-start"}
+							elemGap={"16px"}
+						/>
+						<ActionCards
+							src={noticeImg}
+							msg={"This is Card Two.This is Card Two."}
+							btnTxt={"Go Pro"}
+							btnColor={"#4C95FF"}
+							btnPadding={"1rem 1.2rem"}
+							btnFontSize={"1.2rem"}
+							itemAlignment={"flex-start"}
+							elemGap={"16px"}
+						/>
+					</TwoElementFlexComp>
+				</MiddleSectionWrapper>
+			</>
+			<>
+				<Gradient from={"#fff"} to={"#f8f9fd"}>
+					<SideBarWrapper>
+						<UserDetails
+							avatarSrc={avatarIcon}
+							name={"Floyd Miles"}
+							accountType={"Free account"}
+							iconOneSrc={BellIcon}
+							iconTwoSrc={SettingIcon}
+							badge={"1"}
+						/>
 
-            <SectionWrapper margin={"24px 0 0 0"}>
-              <SectionHeadline>Health</SectionHeadline>
-              <InlineWrapper>
-                <HealthSectionItem
-                  progressPercentage={"75"}
-                  bgColor={"#4C95FF"}
-                  textData={"6:25"}
-                  itemName={"Hours"}
-                  iconSrc={MoonIcon}
-                />
-                <HealthSectionItem
-                  progressPercentage={"100"}
-                  bgColor={"#FF7368"}
-                  textData={"120"}
-                  itemName={"Bpm"}
-                  iconSrc={HeartBeatIcon}
-                />
-                <HealthSectionItem
-                  progressPercentage={"30"}
-                  bgColor={"#6FCE63"}
-                  textData={"1.84"}
-                  itemName={"KCal"}
-                  iconSrc={AppleIcon}
-                />
-              </InlineWrapper>
-              <InfoCard bgColorPrimary={"#FFFBF0"} bgColorSecondary={"#FFEDC4"}>
-                <CardContentWrapper margin={"12px 0 0 0"}>
-                  <CardImage
-                    src={RegisterIcon}
-                    height={"120px"}
-                    width={"120px"}
-                  />
-                  <CenteredCardText>
-                    Sign Up for a personal trainer to improve results
-                    <Chip bgColor={"#FBC852"} button>
-                      Sign Up
-                    </Chip>
-                  </CenteredCardText>
-                </CardContentWrapper>
-              </InfoCard>
-            </SectionWrapper>
+						<SectionWrapper margin={"24px 0 0 0"}>
+							<SectionHeadline>Health</SectionHeadline>
+							<InlineWrapper>
+								<HealthSectionItem
+									progressPercentage={"75"}
+									bgColor={"#4C95FF"}
+									textData={"6:25"}
+									itemName={"Hours"}
+									iconSrc={MoonIcon}
+								/>
+								<HealthSectionItem
+									progressPercentage={"100"}
+									bgColor={"#FF7368"}
+									textData={"120"}
+									itemName={"Bpm"}
+									iconSrc={HeartBeatIcon}
+								/>
+								<HealthSectionItem
+									progressPercentage={"30"}
+									bgColor={"#6FCE63"}
+									textData={"1.84"}
+									itemName={"KCal"}
+									iconSrc={AppleIcon}
+								/>
+							</InlineWrapper>
+							<InfoCard bgColorPrimary={"#FFFBF0"} bgColorSecondary={"#FFEDC4"}>
+								<CardContentWrapper margin={"12px 0 0 0"}>
+									<CardImage
+										src={RegisterIcon}
+										height={"120px"}
+										width={"120px"}
+									/>
+									<CenteredCardText>
+										Sign Up for a personal trainer to improve results
+										<Chip bgColor={"#FBC852"} button>
+											Sign Up
+										</Chip>
+									</CenteredCardText>
+								</CardContentWrapper>
+							</InfoCard>
+						</SectionWrapper>
 
-            <SectionWrapper>
-              <SectionHeadline>Today's trainings</SectionHeadline>
+						<SectionWrapper>
+							<SectionHeadline>Today's trainings</SectionHeadline>
 
-              <InfoCard bgColorPrimary={"#4C95FF"} bgColorSecondary={"#BFD9FF"}>
-                <CardContentWrapper>
-                  <TrainingDetails
-                    title={"Box"}
-                    subtitle={"Sport Club"}
-                    time={"10:00"}
-                  />
-                  <ActivityTimePlan
-                    time={"40"}
-                    unit={"min"}
-                    description={"Warm-up"}
-                  />
-                  <ActivityTimePlan
-                    time={"40"}
-                    unit={"min"}
-                    description={"Stretch"}
-                  />
-                  <ActivityMoreIcon src={ellipsisIcon} />
-                </CardContentWrapper>
-              </InfoCard>
-              <InfoCard bgColorPrimary={"#FF7368"} bgColorSecondary={"#FDBEC0"}>
-                <CardContentWrapper>
-                  <TrainingDetails
-                    title={"Crossfit"}
-                    subtitle={"Sport Club"}
-                    time={"12:00"}
-                  />
-                  <ActivityTimePlan
-                    time={"20"}
-                    unit={"min"}
-                    description={"Warm-up"}
-                  />
-                  <ActivityTimePlan
-                    time={"20"}
-                    unit={"min"}
-                    description={"Pull-Ups"}
-                  />
-                  <ActivityMoreIcon src={ellipsisIcon} />
-                </CardContentWrapper>
-              </InfoCard>
-            </SectionWrapper>
+							<InfoCard bgColorPrimary={"#4C95FF"} bgColorSecondary={"#BFD9FF"}>
+								<CardContentWrapper>
+									<TrainingDetails
+										title={"Box"}
+										subtitle={"Sport Club"}
+										time={"10:00"}
+									/>
+									<ActivityTimePlan
+										time={"40"}
+										unit={"min"}
+										description={"Warm-up"}
+									/>
+									<ActivityTimePlan
+										time={"40"}
+										unit={"min"}
+										description={"Stretch"}
+									/>
+									<ActivityMoreIcon src={ellipsisIcon} />
+								</CardContentWrapper>
+							</InfoCard>
+							<InfoCard bgColorPrimary={"#FF7368"} bgColorSecondary={"#FDBEC0"}>
+								<CardContentWrapper>
+									<TrainingDetails
+										title={"Crossfit"}
+										subtitle={"Sport Club"}
+										time={"12:00"}
+									/>
+									<ActivityTimePlan
+										time={"20"}
+										unit={"min"}
+										description={"Warm-up"}
+									/>
+									<ActivityTimePlan
+										time={"20"}
+										unit={"min"}
+										description={"Pull-Ups"}
+									/>
+									<ActivityMoreIcon src={ellipsisIcon} />
+								</CardContentWrapper>
+							</InfoCard>
+						</SectionWrapper>
 
-            <MoreWrapper>
-              <ActivityMoreIcon src={moreIcon} />
-            </MoreWrapper>
-          </SideBarWrapper>
-        </Gradient>
-        <SideBarWrapper>
-          <UserDetails
-            avatarSrc={avatarIcon}
-            name={"John Kavangh"}
-            accountType={"MMA coach"}
-            iconOneSrc={ChatIcon}
-            online
-          />
-          <UserDetails
-            avatarSrc={avatarIcon}
-            name={"Jacob Jones"}
-            accountType={"Boxing coach"}
-            iconOneSrc={ChatIcon}
-            online
-          />
-        </SideBarWrapper>
-      </>
-    </DashboardLayout>
-  );
+						<MoreWrapper>
+							<ActivityMoreIcon src={moreIcon} />
+						</MoreWrapper>
+					</SideBarWrapper>
+				</Gradient>
+				<SideBarWrapper>
+					<UserDetails
+						avatarSrc={avatarIcon}
+						name={"John Kavangh"}
+						accountType={"MMA coach"}
+						iconOneSrc={ChatIcon}
+						online
+					/>
+					<UserDetails
+						avatarSrc={avatarIcon}
+						name={"Jacob Jones"}
+						accountType={"Boxing coach"}
+						iconOneSrc={ChatIcon}
+						online
+					/>
+				</SideBarWrapper>
+			</>
+		</DashboardLayout>
+	);
 }
 
 export default App;
--- src/components/common/index.js
+++ src/components/common/index.js
@@ -16,335 +16,447 @@ transform: translateY(-12px);
 }
 `;
 export const BaseCard = styled.div`
-  border-radius: 12px;
-  border: none;
-  display: flex;
-  align-items: center;
-  outline-offset: 4px;
-  //background: #fdbec0;
-  margin: 32px 0;
-  background-color: ${(props) => props.bgColor};
+	border-radius: 12px;
+	border: none;
+	display: flex;
+	align-items: center;
+	outline-offset: 4px;
+	//background: #fdbec0;
+	margin: 32px 0;
+	background-color: ${(props) => props.bgColor};
 `;
 export const CardFront = styled.span`
-  display: block;
-  width: 100%;
-  padding: 8px 42px 8px 42px;
-  border-radius: 12px;
-  font-size: 1.25rem;
-  background: rgba(254, 115, 104);
-  color: white;
-  transform: translateY(-12px);
-  border: 2px solid white;
-  margin: 0 -8px;
-  background-color: ${(props) => props.bgColor};
-  animation: ${rise} 500ms linear forwards;
-  :hover {
-    animation: ${settle} 500ms linear forwards;
-  }
+	display: block;
+	width: 100%;
+	padding: 8px 42px 8px 42px;
+	border-radius: 12px;
+	font-size: 1.25rem;
+	background: rgba(254, 115, 104);
+	color: white;
+	transform: translateY(-12px);
+	border: 2px solid white;
+	margin: 0 -8px;
+	background-color: ${(props) => props.bgColor};
+	animation: ${rise} 500ms linear forwards;
+	:hover {
+		animation: ${settle} 500ms linear forwards;
+	}
 `;
 
 export const SquareButton = styled.button`
-  padding: 0;
-  outline: 0;
-  border-radius: 12px;
-  width: 40px;
-  height: 40px;
-  background: url(${(props) => props.backgroundImage});
-  background-position: center;
-  background-repeat: no-repeat;
-  border: 1px #eee solid;
-  cursor: pointer;
-  position: relative;
+	padding: 0;
+	outline: 0;
+	border-radius: 12px;
+	width: 40px;
+	height: 40px;
+	background: url(${(props) => props.backgroundImage});
+	background-position: center;
+	background-repeat: no-repeat;
+	border: 1px #eee solid;
+	cursor: pointer;
+	position: relative;
 `;
 export const SideBarWrapper = styled.div`
-  padding: 0 24px;
-  display: flex;
-  flex-direction: column;
-  justify-content: space-between;
+	padding: 0 24px;
+	display: flex;
+	flex-direction: column;
+	justify-content: space-between;
 `;
 export const SectionWrapper = styled.div`
-  margin: ${(props) => props.margin || "0"};
+	margin: ${(props) => props.margin || "0"};
 `;
 export const UserAvatar = styled.div`
-  border-radius: 50%;
-  height: 50px;
-  width: 50px;
-  background: url(${(props) => props.avatarSrc});
-  background-position: center;
-  background-repeat: no-repeat;
-  position: relative;
+	border-radius: 50%;
+	height: 50px;
+	width: 50px;
+	background: url(${(props) => props.avatarSrc});
+	background-position: center;
+	background-repeat: no-repeat;
+	position: relative;
 `;
 export const ProgressBarBase = styled.div`
-  background: #eef1fa;
-  width: 100%;
-  border-radius: 12px;
-  height: 5px;
+	background: #eef1fa;
+	width: 100%;
+	border-radius: 12px;
+	height: 5px;
 `;
 export const Progress = styled.div`
-  width: ${(props) => props.progressPercentage}%;
-  background: ${(props) => props.barColor};
-  height: 100%;
-  border-radius: 12px;
+	width: ${(props) => props.progressPercentage}%;
+	background: ${(props) => props.barColor};
+	height: 100%;
+	border-radius: 12px;
 `;
 export const SectionHeadline = styled.div`
-  font-size: 1.4rem;
-  font-weight: 700;
+	font-size: 1.4rem;
+	font-weight: 700;
 `;
 export const StatImage = styled.div`
-  border-radius: 8px;
-  background-image: url(${(props) => props.imgSrc});
-  background-position: center;
-  background-repeat: no-repeat;
-  background-size: 20px;
-  background-color: ${(props) => props.bgColor};
-  width: 35px;
-  height: 35px;
+	border-radius: 8px;
+	background-image: url(${(props) => props.imgSrc});
+	background-position: center;
+	background-repeat: no-repeat;
+	background-size: 20px;
+	background-color: ${(props) => props.bgColor};
+	width: 35px;
+	height: 35px;
 `;
 export const CardImage = styled.img`
-  height: ${(props) => props.height};
-  width: ${(props) => props.width};
+	height: ${(props) => props.height};
+	width: ${(props) => props.width};
 `;
 export const CenteredCardText = styled.div`
-  text-align: center;
-  max-width: 200px;
-  word-wrap: normal;
-  color: #757582;
-  font-size: 1rem;
-  display: flex;
-  flex-direction: column;
-  font-weight: 600;
+	text-align: center;
+	max-width: 200px;
+	word-wrap: normal;
+	color: #757582;
+	font-size: 1rem;
+	display: flex;
+	flex-direction: column;
+	font-weight: 600;
 `;
 export const InlineWrapper = styled.div`
-  display: flex;
-  > span {
-    position: relative;
-    padding: 0.5rem 1rem;
-    display: flex;
-    text-align: center;
-    justify-content: center;
-    align-items: center;
-    flex: 0 1 33%;
-    @media only screen and (max-width: 768px) {
-      padding: 0.5rem 0.6rem;
-    }
-  }
-  > span:not(:last-child)::after {
-    content: "";
-    position: absolute;
-    right: -2px;
-    top: 10%;
-    width: 2px;
-    height: 80%;
-    background-color: #eee;
-  }
-  > span:first-child {
-    padding-left: 0;
-  }
+	display: flex;
+	> span {
+		position: relative;
+		padding: 0.5rem 1rem;
+		display: flex;
+		text-align: center;
+		justify-content: center;
+		align-items: center;
+		flex: 0 1 33%;
+		@media only screen and (max-width: 768px) {
+			padding: 0.5rem 0.6rem;
+		}
+	}
+	> span:not(:last-child)::after {
+		content: "";
+		position: absolute;
+		right: -2px;
+		top: 10%;
+		width: 2px;
+		height: 80%;
+		background-color: #eee;
+	}
+	> span:first-child {
+		padding-left: 0;
+	}
 `;
 export const HealthStatWrapper = styled.span`
-  display: flex;
-  flex-direction: column;
-  gap: 12px;
+	display: flex;
+	flex-direction: column;
+	gap: 12px;
 `;
 export const HealthStatData = styled.div`
-  font-size: 2.5rem;
-  font-weight: 500;
+	font-size: 2.5rem;
+	font-weight: 500;
 `;
 export const HealthStatDetail = styled.div`
-  display: flex;
-  gap: 12px;
-  align-items: center;
-  margin: 12px 5px;
+	display: flex;
+	gap: 12px;
+	align-items: center;
+	margin: 12px 5px;
 `;
 export const CardContentWrapper = styled.div`
-  display: flex;
-  justify-content: space-between;
-  align-items: center;
-  margin: ${(props) => props.margin || "0"};
+	display: flex;
+	justify-content: space-between;
+	align-items: center;
+	margin: ${(props) => props.margin || "0"};
 `;
 export const ActivityMoreIcon = styled.img`
-  height: 20px;
-  width: 20px;
+	height: 20px;
+	width: 20px;
 `;
 export const MoreWrapper = styled.div`
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  width: 100%;
+	display: flex;
+	justify-content: center;
+	align-items: center;
+	width: 100%;
 `;
 export const Gradient = styled.div`
-  background: ${(props) => `linear-gradient(
+	background: ${(props) => `linear-gradient(
     180deg,
     ${props.from} 0%,
     ${props.to} 100%
   )`};
-  border-radius: 0 0 16px 16px;
+	border-radius: 0 0 16px 16px;
 `;
 export const Chip = styled.div`
-  padding: 0.5rem;
-  margin: 12px auto;
-  display: inline-block;
-  background: ${(props) => props.bgColor};
-  ${(props) =>
-    props.button
-      ? `
+	padding: 0.5rem;
+	margin: 12px auto;
+	display: inline-block;
+	background: ${(props) => props.bgColor};
+	${(props) =>
+		props.button
+			? `
   cursor:pointer;
   transition:1s;
   :hover{
     color:black;
   }
   `
-      : null}
-  border-radius:8px;
-  font-size: 0.8rem;
-  font-weight: 700;
-  color: ${(props) => props.color || "#fff"};
+			: null}
+	border-radius:8px;
+	font-size: 0.8rem;
+	font-weight: 700;
+	color: ${(props) => props.color || "#fff"};
 `;
 
 export const TrainingTitle = styled.div`
-  font-size: 1.2rem;
-  margin: 8px 0 8px 0;
-  text-align: left;
-  font-weight: 600;
-  color: #fff;
+	font-size: 1.2rem;
+	margin: 8px 0 8px 0;
+	text-align: left;
+	font-weight: 600;
+	color: #fff;
 `;
 export const TrainingSubTitle = styled(TrainingTitle)`
-  font-size: 0.9rem;
-  margin: 0px 0;
-  font-weight: bold;
+	font-size: 0.9rem;
+	margin: 0px 0;
+	font-weight: bold;
 `;
 
 export const ActivityTime = styled.div`
-  text-align: center;
-  font-size: 2.5rem;
-  color: #fff;
+	text-align: center;
+	font-size: 2.5rem;
+	color: #fff;
 `;
 export const ActivityUnit = styled(ActivityTime)`
-  font-size: 1rem;
-  font-weight: 700;
+	font-size: 1rem;
+	font-weight: 700;
 `;
 export const ActivityDescription = styled(ActivityUnit)`
-  font-weight: 500;
+	font-weight: 500;
 `;
 export const InfoCard = ({ children, bgColorPrimary, bgColorSecondary }) => {
-  return (
-    <BaseCard bgColor={bgColorSecondary}>
-      <CardFront bgColor={bgColorPrimary}>{children}</CardFront>
-    </BaseCard>
-  );
+	return (
+		<BaseCard bgColor={bgColorSecondary}>
+			<CardFront bgColor={bgColorPrimary}>{children}</CardFront>
+		</BaseCard>
+	);
 };
 export const Badge = styled.div`
-  height: 14px;
-  width: 15px;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  font-size: 8px;
-  border-radius: 50%;
-  color: #fff;
-  background: ${(props) => (props.type === "status" ? "#BDFFA9" : "#eb3232")};
-  position: absolute;
-  ${(props) =>
-    props.type === "status"
-      ? `
+	height: 14px;
+	width: 15px;
+	display: flex;
+	justify-content: center;
+	align-items: center;
+	font-size: 8px;
+	border-radius: 50%;
+	color: #fff;
+	background: ${(props) => (props.type === "status" ? "#BDFFA9" : "#eb3232")};
+	position: absolute;
+	${(props) =>
+		props.type === "status"
+			? `
       bottom:0;
       left:-4px;
       `
-      : `  
+			: `  
     top: 0;
     right: -4px;
   `};
 `;
 const HealthItemWrapper = styled.span`
-  display: flex;
-  flex-direction: column;
+	display: flex;
+	flex-direction: column;
 `;
 const UserDetailsWrapper = styled.div``;
 const UserName = styled.div`
-  font-weight: 700;
+	font-weight: 700;
 `;
 const AccountType = styled.div`
-  font-size: 0.8rem;
+	font-size: 0.8rem;
 `;
 const UserInfo = styled.div`
-  display: flex;
-  align-items: center;
-  gap: 18px;
-  margin-right: 18px;
+	display: flex;
+	align-items: center;
+	gap: 18px;
+	margin-right: 18px;
 `;
+
+export const ActionButton = styled.a`
+	text-decoration: none;
+	padding: ${(props) => props.padding || "1rem"};
+	font-size: ${(props) => props.fontSize || "1rem"};
+	width: fit-content;
+	font-weight: 700;
+	border-radius: 24px;
+	cursor: pointer;
+	${(props) => {
+		if (props.outlined) {
+			return `
+				color: ${props.color || "#000"};
+				border: 2px solid ${props.color || "#ff0000"};
+				
+			`;
+		}
+		if (props.filled) {
+			return `
+				background: ${props.color || "#fff"};
+				color: #fff;
+				border: 2px solid ${props.color || "#ff0000"};
+			`;
+		}
+	}}
+`;
+
+export const MiddleSectionWrapper = styled.div`
+	height: 100%;
+	background-color: #f8f9fd;
+	display: flex;
+	flex-direction: column;
+	justify-content: space-between;
+	padding: 1rem 3rem;
+`;
+
+export const TwoElementFlexComp = styled.div`
+	display: flex;
+	justify-content: space-between;
+	align-items: ${(props) => props.itemAlignment};
+	gap: ${(props) => props.gap || 0};
+	flex-direction: ${(props) => (props.column ? "column" : "row")};
+`;
+
+export const Card = styled.div`
+	background-color: #fff;
+	border-radius: 12px;
+	padding: 1.5rem;
+`;
+
+export const Img = styled.img`
+	width: 100%;
+	height: auto;
+`;
+export const ImgWrapper = styled.div`
+	flex: 0 1 60%;
+	display: grid;
+	place-items: center;
+`;
+
+//  <-- ---------------------------- Components Start here ----------------------------  -->  //
 export const ProgressBar = ({ progressPercentage, barColor }) => {
-  return (
-    <ProgressBarBase>
-      <Progress progressPercentage={progressPercentage} barColor={barColor} />
-    </ProgressBarBase>
-  );
+	return (
+		<ProgressBarBase>
+			<Progress progressPercentage={progressPercentage} barColor={barColor} />
+		</ProgressBarBase>
+	);
 };
 
 export const HealthSectionItem = ({
-  progressPercentage,
-  bgColor,
-  textData,
-  itemName,
-  iconSrc,
+	progressPercentage,
+	bgColor,
+	textData,
+	itemName,
+	iconSrc,
 }) => {
-  return (
-    <HealthItemWrapper>
-      <HealthStatData>{textData}</HealthStatData>
-      <HealthStatDetail>
-        <StatImage imgSrc={iconSrc} bgColor={bgColor} />
-        {itemName}
-      </HealthStatDetail>
-      <ProgressBar progressPercentage={progressPercentage} barColor={bgColor} />
-    </HealthItemWrapper>
-  );
+	return (
+		<HealthItemWrapper>
+			<HealthStatData>{textData}</HealthStatData>
+			<HealthStatDetail>
+				<StatImage imgSrc={iconSrc} bgColor={bgColor} />
+				{itemName}
+			</HealthStatDetail>
+			<ProgressBar progressPercentage={progressPercentage} barColor={bgColor} />
+		</HealthItemWrapper>
+	);
 };
 
 export const TrainingDetails = ({ title, subtitle, time }) => {
-  return (
-    <div>
-      <TrainingTitle>{title}</TrainingTitle>
-      <TrainingSubTitle>{subtitle}</TrainingSubTitle>
-      <Chip bgColor={"#FFF"} color={"#888996"}>
-        {time}
-      </Chip>
-    </div>
-  );
+	return (
+		<div>
+			<TrainingTitle>{title}</TrainingTitle>
+			<TrainingSubTitle>{subtitle}</TrainingSubTitle>
+			<Chip bgColor={"#FFF"} color={"#888996"}>
+				{time}
+			</Chip>
+		</div>
+	);
 };
 
 export const ActivityTimePlan = ({ time, unit, description }) => {
-  return (
-    <div>
-      <ActivityTime>{time}</ActivityTime>
-      <ActivityUnit>{unit}</ActivityUnit>
-      <ActivityDescription>{description}</ActivityDescription>
-    </div>
-  );
+	return (
+		<div>
+			<ActivityTime>{time}</ActivityTime>
+			<ActivityUnit>{unit}</ActivityUnit>
+			<ActivityDescription>{description}</ActivityDescription>
+		</div>
+	);
 };
 
 export const UserDetails = ({
-  avatarSrc,
-  name,
-  accountType,
-  iconOneSrc,
-  iconTwoSrc,
-  badge,
-  online,
+	avatarSrc,
+	name,
+	accountType,
+	iconOneSrc,
+	iconTwoSrc,
+	badge,
+	online,
 }) => {
-  return (
-    <CardContentWrapper margin={"16px 0"}>
-      <UserInfo>
-        <UserAvatar avatarSrc={avatarSrc}>
-          {online ? <Badge type="status" /> : null}
-        </UserAvatar>
-        <UserDetailsWrapper>
-          <UserName>{name}</UserName>
-          <AccountType>{accountType}</AccountType>
-        </UserDetailsWrapper>
-      </UserInfo>
-      <SquareButton backgroundImage={iconOneSrc}>
-        {badge ? <Badge>{badge}</Badge> : null}
-      </SquareButton>
-      {iconTwoSrc ? <SquareButton backgroundImage={iconTwoSrc} /> : null}
-    </CardContentWrapper>
-  );
+	return (
+		<CardContentWrapper margin={"16px 0"}>
+			<UserInfo>
+				<UserAvatar avatarSrc={avatarSrc}>
+					{online ? <Badge type='status' /> : null}
+				</UserAvatar>
+				<UserDetailsWrapper>
+					<UserName>{name}</UserName>
+					<AccountType>{accountType}</AccountType>
+				</UserDetailsWrapper>
+			</UserInfo>
+			<SquareButton backgroundImage={iconOneSrc}>
+				{badge ? <Badge>{badge}</Badge> : null}
+			</SquareButton>
+			{iconTwoSrc ? <SquareButton backgroundImage={iconTwoSrc} /> : null}
+		</CardContentWrapper>
+	);
+};
+
+export const SearchBarCalendarComp = () => {
+	return (
+		<TwoElementFlexComp>
+			<div>Searchbar</div>
+			<div>Calendar</div>
+		</TwoElementFlexComp>
+	);
+};
+export const MiddleHeaderComp = () => {
+	return (
+		<TwoElementFlexComp>
+			<TwoElementFlexComp column>
+				<h1>Hello Floyd!</h1>
+				<h3>Here is your daily Statistics</h3>
+			</TwoElementFlexComp>
+			<div>two blocks</div>
+		</TwoElementFlexComp>
+	);
+};
+
+export const ActionCards = ({
+	src,
+	msg,
+	btnPadding,
+	btnFontSize,
+	btnTxt,
+	btnColor,
+	itemAlignment,
+	elemGap,
+}) => {
+	return (
+		<Card>
+			<TwoElementFlexComp gap={elemGap}>
+				<ImgWrapper>
+					<Img src={src} />
+				</ImgWrapper>
+				<TwoElementFlexComp itemAlignment={itemAlignment} column>
+					<h3>{msg}</h3>
+					<ActionButton
+						padding={btnPadding}
+						fontSize={btnFontSize}
+						color={btnColor}
+						outlined>
+						{btnTxt}
+					</ActionButton>
+				</TwoElementFlexComp>
+			</TwoElementFlexComp>
+		</Card>
+	);
 };
